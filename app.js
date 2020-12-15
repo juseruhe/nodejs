a8051c38880011add6e31bb88fab56a53dd4a56b
Sebastian <jsruiz7411@misena.edu.co>
2020-12-15 16:34:26 -0500

    cambios

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+// Variables Exportables y Cowsay
+/*
 const {frutas,dinero} = require("./frutas")
 
 const cowsay = require("cowsay")
@@ -14,4 +15,60 @@ frutas.forEach(item => {
 console.count(item)
 })
 
-console.log(dinero)
+console.log(dinero)*/
+
+// Servidor HTTP para funcionar en Localhost
+/* const http = require('http');
+
+const server = http.createServer((req,res)=>{
+
+res.end("Estoy respondiendo a su solicitud v.3");
+})
+
+
+const puerto=3000;
+
+server.listen(puerto,()=>{
+
+    console.log("Escuchando solicitudes");
+})*/
+
+
+
+
+// Usar Express js
+
+const express = require('express')
+const app = express();
+
+const port = 3000;
+
+// Plantilla
+app.set('view engine', 'ejs');
+
+
+
+app.use(express.static(__dirname+"/public"))
+
+
+app.get('/', (req,res) => {
+  //  console.log(__dirname)
+    res.send("Mi respuesta desde Express V.2");
+})
+
+app.get('/servicios', (req,res) => {
+
+    res.send("Estás en la Página de Servicios");
+})
+
+app.use((req,res,next) => {
+
+    res.status(404).sendFile(__dirname+"/public/404.html")
+})
+ 
+app.listen(port, () =>{
+
+    console.log("Servidor a su servicio en el puerto ",port);
+})
+
+
