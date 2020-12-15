4d481507d99396a9131e73c013ac56b3e6554758
Sebastian <jsruiz7411@misena.edu.co>
2020-12-15 18:10:12 -0500

    hubo cambios

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ server.listen(puerto,()=>{
 const express = require('express')
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT|| 3000;
 
 // Plantilla
 app.set('view engine', 'ejs');
