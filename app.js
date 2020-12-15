8ce13410d022d05ebf575e90108884bf085675f0
Sebastian <jsruiz7411@misena.edu.co>
2020-12-15 15:21:03 -0500

    instalar cowsay

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 
 const {frutas,dinero} = require("./frutas")
 
+const cowsay = require("cowsay")
+
+console.log(cowsay.say({
+    text : "Hola a todos",
+	e : "oO",
+	T : "U "
+}));
 
 
 frutas.forEach(item => {
