0e69658e760b72ee38bfaac3b8547d49d649394e
Sebastian <jsruiz7411@misena.edu.co>
2020-12-18 14:52:08 -0500

    cambios

--- router/mascotas.js
+++ router/mascotas.js
@@ -29,7 +29,28 @@ router.get('/',async (req,res) => {
 })
 
 
+router.get('/crear',(req,res) =>{
 
+    res.render("crear")
+})
+
+router.post('/',async(req,res) =>{
+
+    const body = req.body
+    console.log(body)
+
+    try {
+
+        await Mascota.create(body)
+
+        res.redirect('/mascotas')
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+
+})
 
 
 
